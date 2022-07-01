a37f3b37fc60b0de99c8aa5ad7529228b67e6e55
yeh-sudo <75301735+yeh-sudo@users.noreply.github.com>
2022-07-01 15:28:14 +0800

    Time: 30 ms (54.58%), Space: 16.9 MB (86.05%) - LeetHub

--- 136-single-number/136-single-number.cpp
+++ 136-single-number/136-single-number.cpp
@@ -2,20 +2,8 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         int ans = 0;
-        map<int, int> cnt;
         for (int i = 0; i < nums.size(); i++){
-            if (cnt.find(nums[i]) == cnt.end()){
-                cnt.insert(make_pair(nums[i], 1));
-            }
-            else {
-                cnt[nums[i]]++;
-            }
-        }
-        for (auto& p : cnt){
-            if (p.second == 1){
-                ans = p.first;
-                break;
-            }
+            ans ^= nums[i];
         }
         return ans;
     }
