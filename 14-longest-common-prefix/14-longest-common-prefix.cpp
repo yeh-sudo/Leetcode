a2b95e9163fb84f72886b317a17fc5c734d92bfb
yeh-sudo <75301735+yeh-sudo@users.noreply.github.com>
2022-07-01 15:00:13 +0800

    Time: 3 ms (91.75%), Space: 9.1 MB (93.62%) - LeetHub

---
+++ 14-longest-common-prefix/14-longest-common-prefix.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string ans = "";
+            bool check = true;
+            int leng = INT_MAX, pos;
+            for (int i = 0; i < strs.size(); i++){
+                int t = strs[i].size();
+                if (t <= leng){
+                    leng = min(leng, t);
+                    pos = i;
+                }
+            }
+
+            for (int i = 0; i < leng; i++){
+                for (int j = 1; j < strs.size(); j++){
+                    if (strs[j][i] != strs[j-1][i]){
+                        check = false;
+                        break;
+                    }
+                }
+                if (check)
+                    ans += strs[pos][i];
+                else
+                    break;
+            }
+            return ans;
+    }
+};
