c26503b32545a8c15dd476f1a3b6daa91c653850
yeh-sudo <75301735+yeh-sudo@users.noreply.github.com>
2022-07-23 17:19:03 +0800

    Time: 1475 ms (17.19%), Space: 154.2 MB (52.93%) - LeetHub

--- 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.cpp
+++ 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.cpp
@@ -1,56 +1,22 @@
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+template<class T> using oset = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+// less: it is the basic for comparison of two function. Use less_equal for ordered multiset.
+// order_of_key(k): Number of items strictly smaller than k
+// find_by_order(k): kth element in a set (counting from zero) 
+
 class Solution {
 public:
-    vector<int> v;
-    vector<long long> tree;
-
-    void build(int idx, int l, int r){
-        if (l + 1 == r){
-            tree[idx] = v[l];
-            return;
-        }
-        int mid = l + (r - l) / 2;
-        build(2 * idx, l, mid);
-        build(2 * idx + 1, mid, r);
-        tree[idx] = tree[2 * idx] + tree[2 * idx + 1];
-    }
-    
-    void modify(int p, int x, int idx, int l, int r){
-        if (l + 1 == r){
-            v[p] = x;
-            tree[idx] = x;
-            return;
-        }
-        int mid = l + (r - l) / 2;
-        if (p < mid){
-            modify(p, x, 2 * idx, l, mid);
-        }
-        else {
-            modify(p, x, 2 * idx + 1, mid, r);
-        }
-        tree[idx] = tree[2 * idx] + tree[2 * idx + 1];
-    }
-    
-    int query(int x, int y, int idx, int l, int r){
-        if (x >= r || l >= y) return 0;
-        if (x <= l && r <= y) return tree[idx];
-        int mid = l + (r - l) / 2;
-        return query(x, y, 2 * idx, l, mid) + query(x, y, 2 * idx + 1, mid, r);
-    }
-    
     vector<int> countSmaller(vector<int>& nums) {
+        oset<int> st;
+        vector<int> ans;
         int n = nums.size();
-        vector<int> ans(n);
-        v.resize(n+1, 0);
-        tree.resize(4 * n + 1);
-        vector<int> tmp = nums, cnt(n);
-        sort(tmp.begin(), tmp.end());
-        for (int i = 0; i < n; i++){
-            cnt[i] = lower_bound(tmp.begin(), tmp.end(), nums[i]) - tmp.begin();
+        for(int i=n-1; i>=0; --i){
+            ans.push_back(st.order_of_key(nums[i]));
+            st.insert(nums[i]);
         }
-        for (int i = n - 1; i >= 0; i--){
-            ans[i] = query(0, cnt[i], 1, 0, n);
-            modify(cnt[i], v[cnt[i]]+1, 1, 0, n);
-        }
+        reverse(ans.begin(), ans.end());
         return ans;
     }
 };
