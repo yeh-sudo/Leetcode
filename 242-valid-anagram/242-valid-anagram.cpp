e11b8b140a4fcc68f4ee468ffcde79ff2322d6aa
yeh-sudo <75301735+yeh-sudo@users.noreply.github.com>
2022-07-28 14:26:14 +0800

    Time: 9 ms (78.39%), Space: 7.3 MB (77.97%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -1,8 +1,17 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-        return s == t;
+        vector<int> cnt(26, 0);
+        for (int i = 0; i < s.size(); i++){
+            cnt[s[i]-'a']++;
+        }
+        for (int i = 0; i < t.size(); i++){
+            cnt[t[i]-'a']--;
+        }
+        bool ans = 1;
+        for (int i = 0; i < 26; i++){
+            ans &= (cnt[i] == 0);
+        }
+        return ans;
     }
 };
