28330e72fabad0fe06dad64420e5adc4f984f9f0
yeh-sudo <75301735+yeh-sudo@users.noreply.github.com>
2022-08-13 17:08:21 +0800

    Time: 1362 ms (24.09%), Space: 339.5 MB (17.22%) - LeetHub

--- 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.cpp
+++ 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.cpp
@@ -1,36 +1,38 @@
 class Solution {
 public:
+    
+    bool checkSubstring(unordered_map<string, int> wordCount, string s, int wordLen) {
+        for(int j=0; j<s.size(); j+=wordLen) {
+            string w = s.substr(j, wordLen);
+            if(wordCount.find(w) != wordCount.end()) {
+                if(--wordCount[w] == -1) {
+                    return false;
+                }
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+
     vector<int> findSubstring(string s, vector<string>& words) {
-        vector<int> ans;
-        int len = words.size() * words[0].size();
-        int wlen = words[0].size();
-        unordered_map<string, int> h;
-        for (int i = 0; i < words.size(); i++){
-            h[words[i]]++;
+        vector<int> res;
+        int wordLen = words[0].size();
+        int sLen = s.size();
+        int wordsWindow = words.size() * wordLen;
+        
+        unordered_map<string, int> wordCount;
+        for(int i=0; i<words.size(); i++) {
+            wordCount[words[i]]++;
         }
-        if (s.size() < len){
-            return ans;
+        
+        int i = 0;
+        while(i + wordsWindow <= sLen) {
+            if(checkSubstring(wordCount, s.substr(i, wordsWindow), wordLen)) {
+                res.push_back(i);
+            }
+            i++;
         }
-        for (int i = 0; i <= s.size() - len; i++){
-            string str = s.substr(i, len);
-            unordered_map<string, int> tmp = h;
-            for (int k = 0; k < str.size(); k += wlen){
-                string ss = str.substr(k, wlen);
-                if (tmp.find(ss) != tmp.end()){
-                    tmp[ss]--;
-                }
-            }
-            bool valid = 1;
-            for (auto p : tmp){
-                if (p.second != 0){
-                    valid = 0;
-                    break;
-                }
-            }
-            if (valid){
-                ans.push_back(i);
-            }
-        }
-        return ans;
+        return res;
     }
 };
