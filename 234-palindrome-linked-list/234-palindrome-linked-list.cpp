34c6284e03f2be23be2691fe8fa8d3aa8f526582
yeh-sudo <75301735+yeh-sudo@users.noreply.github.com>
2022-08-23 12:32:21 +0800

    Time: 338 ms (60.99%), Space: 121.7 MB (34.41%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/234-palindrome-linked-list.cpp
@@ -1,38 +1,15 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     bool isPalindrome(ListNode* head) {
-        vector<int> a;
-        ListNode* cur = head;
-        while (cur != NULL){
-            a.push_back(cur->val);
-            cur = cur->next;
+        string front = "";
+        while(head){
+            front += head->val;
+            head = head->next;
         }
-        if (a.size() % 2 == 0){
-            int l = a.size() / 2 - 1;
-            int r = l + 1;
-            while (l >= 0 && r < a.size()){
-                if (a[l] != a[r]) return 0;
-                l--, r++;
-            }
-        }
-        else {
-            int l = a.size() / 2 - 1;
-            int r = l + 2;
-            while (l >= 0 && r < a.size()){
-                if (a[l] != a[r]) return 0;
-                l--, r++;
-            }
-        }
-        return 1;
+        string reverse_front = front;
+        reverse(front.begin(), front.end());
+        if(front == reverse_front)
+            return true;
+        return false;
     }
 };
