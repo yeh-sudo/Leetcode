ab17c4f7544137216091695b8c29b899e3d11abb
yeh-sudo <75301735+yeh-sudo@users.noreply.github.com>
2022-08-23 12:32:43 +0800

    Time: 401 ms (41.44%), Space: 121.9 MB (33.33%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/234-palindrome-linked-list.cpp
@@ -2,13 +2,13 @@ class Solution {
 public:
     bool isPalindrome(ListNode* head) {
         string front = "";
-        while(head){
+        while (head){
             front += head->val;
             head = head->next;
         }
         string reverse_front = front;
         reverse(front.begin(), front.end());
-        if(front == reverse_front)
+        if (front == reverse_front)
             return true;
         return false;
     }
