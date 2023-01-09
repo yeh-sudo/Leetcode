9fc5d803167375dbb623d2dd563d92c146027f65
yeh-sudo <75301735+yeh-sudo@users.noreply.github.com>
2023-01-09 22:00:01 +0800

    Time: 2 ms (60.20%), Space: 8.3 MB (93.88%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
@@ -1,27 +1,33 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
-    vector<int> ans;
-    void dfs(TreeNode *node){
-        if (node == NULL){
-            return;
-        }
-        ans.push_back(node->val);
-        dfs(node->left);
-        dfs(node->right);
-    }
 public:
+    TreeNode* rightMostFinder(TreeNode *cur, TreeNode *root){
+        while (cur->right && cur->right != root){
+            cur=cur->right;
+        }
+        return cur;
+    }
     vector<int> preorderTraversal(TreeNode* root) {
-        dfs(root);
+        TreeNode*cur = root;
+        vector<int> ans;
+        while (cur){
+            TreeNode *leftNode = cur->left;
+            if (leftNode){
+                TreeNode *RightMost = rightMostFinder(leftNode, cur);
+                if (RightMost->right){
+                    RightMost->right = nullptr;
+                    cur=cur->right;
+                }
+                else {
+                    RightMost->right = cur;
+                    ans.push_back(cur->val);
+                    cur = cur->left;
+                }
+            }
+            else {
+                ans.push_back(cur->val);
+                cur = cur->right;
+            }
+        }
         return ans;
     }
 };
